2cbbfd0c45cb8f2484ffd8137b343f8b3d41f11e
Ephraim Becker <emb16@outlook.com>
2022-08-05 15:22:31 -0400

    Added TrackList component.

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { TrackList } from './TrackList';
+import { TrackList } from '../TrackList/TrackList';
 
 export class PlayList extends React.Component {
     render() {
