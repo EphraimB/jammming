fff1de78d785e91b98760dd581300f8f135fb7a2
Ephraim Becker <emb16@outlook.com>
2022-08-05 17:11:56 -0400

    Added return statement in TrackList.

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -3,9 +3,11 @@ import { track } from '../Track/Track';
 
 export class TrackList extends React.Component {
     render() {
-        <div className="TrackList">
-            { /* You will add a map method that renders a set of Track components */ }
-        </div>
+        return (
+            <div className="TrackList">
+                { /* You will add a map method that renders a set of Track components */ }
+            </div>
+        )
     }
 }
 
