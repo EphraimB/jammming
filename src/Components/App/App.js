7c37b96e397db19735b9c311945d72484993259e
Ephraim Becker <emb16@outlook.com>
2022-08-10 12:01:52 -0400

    Added a removeTrack method.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -52,10 +52,12 @@ class App extends React.Component {
       ]
     }
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   addTrack(track) {
     const tracks = this.state.playlistTracks;
+
     if(tracks.find(savedTrack => savedTrack.id === track.id)) {
       return;
     }
@@ -63,6 +65,14 @@ class App extends React.Component {
     tracks.push(track);
 
     this.setState({playlistTracks: tracks});
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+    this.setState({ playlistTracks: tracks });
   }
 
   render() {
@@ -73,7 +83,7 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-            <PlayList playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
+            <PlayList playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />
           </div>
         </div>
       </div>
